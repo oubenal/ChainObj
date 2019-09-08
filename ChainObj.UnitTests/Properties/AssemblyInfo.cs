53171dd91d9ff85b76899e34046c6a511c961a0c
Redouane Oubenal <oubenal@gmail.com>
2019-09-08 17:00:27 +0200

    add unit test

---
+++ ChainObj.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChainObj.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChainObj.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e7bdb62-1737-4323-96d5-210e7511e4ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ChainObj.UnitTests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChainObj.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEmptyDB()
+        {
+            using (var db = new Database($@"C:\temp\{Guid.NewGuid().ToString("n").Substring(0, 8)}", true))
+                Assert.IsNull(db.Get(0));
+        }
+        [TestMethod]
+        public void TestInsertAndRetrieveInDB()
+        {
+            using (var db = new Database($@"C:\temp\{Guid.NewGuid().ToString("n").Substring(0, 8)}", true))
+            {
+                db.Put(0, "data");
+                Assert.IsTrue(db.Get(0).Equals("data"));
+            }
+        }
+        [TestMethod]
+        public void TestRetrieveAllSortedInDB()
+        {
+            using (var db = new Database($@"C:\temp\{Guid.NewGuid().ToString("n").Substring(0, 8)}", true))
+            {
+                for (var i = 0; i < 21; i++)
+                    db.Put(i, $"New block {i}");
+                var listElmt = db.All();
+                var ind = 0;
+                foreach (var elmt in listElmt)
+                    Assert.IsTrue(elmt.Equals($"New block {ind++}"));
+            }
+        }
+        [TestMethod]
+        public void TestRetrieveLastInDB()
+        {
+            using (var db = new Database($@"C:\temp\{Guid.NewGuid().ToString("n").Substring(0, 8)}", true))
+            {
+                var lastIndex = 20;
+                for (var i = 0; i <= lastIndex; i++)
+                    db.Put(i, $"New block {i}");
+                var lastElmt = db.GetLast();
+                Assert.IsTrue(lastElmt.Equals($"New block {lastIndex}"));
+            }
+        }
+    }
+}
