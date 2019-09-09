0c4ee9672803d25506998a3a789eb235dca9e6bf
Redouane Oubenal <oubenal@gmail.com>
2019-09-09 06:21:04 +0200

    add TempDirectory class

--- ChainObj.UnitTests/BlockchainTests.cs
+++ ChainObj.UnitTests/BlockchainTests.cs
@@ -3,56 +3,88 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ChainObj.UnitTests
 {
+    class TempDirectory : IDisposable
+    {
+        internal string Path { get; }
+        internal TempDirectory()
+        {
+            Path = Utilities.TempDirPath;
+            if (System.IO.Directory.Exists(Path))
+                System.IO.Directory.Delete(Path, true);
+            System.IO.Directory.CreateDirectory(Path);
+        }
+        public void Dispose()
+        {
+            System.IO.Directory.Delete(Path, true);
+        }
+    }
     [TestClass]
     public class BlockchainTests
     {
-        private static Blockchain CreateBlockchain()
-        {
-            var dbPath = Utilities.TempDirPath;
-            if (System.IO.Directory.Exists(dbPath))
-                System.IO.Directory.Delete(dbPath, true);
-
-            var blockchain = new Blockchain(dbPath);
-            return blockchain;
-        }
         [TestMethod]
         public void TestEmptyBlockchain()
         {
-            var blockchain = CreateBlockchain();
-            Assert.AreEqual(0, blockchain.LastBlock.Height);
-            Assert.IsNull(blockchain.LastBlock.PreviousHash);
-            Assert.AreEqual("GenesisBlock", blockchain.LastBlock.Data);
+            using (var tmpDir = new TempDirectory())
+            {
+                var blockchain = new Blockchain(tmpDir.Path);
+                Assert.AreEqual(0, blockchain.LastBlock.Height);
+                Assert.IsNull(blockchain.LastBlock.PreviousHash);
+                Assert.AreEqual("GenesisBlock", blockchain.LastBlock.Data);
+            }
         }
         
         [TestMethod]
         public void TestInsertBlock()
         {
-            var blockchain = CreateBlockchain();
-            blockchain.InsertBlock("New data");
-            Assert.AreEqual(1, blockchain.LastBlock.Height);
-            Assert.IsNotNull(blockchain.LastBlock.PreviousHash);
-            Assert.AreEqual("New data", blockchain.LastBlock.Data);
+            using (var tmpDir = new TempDirectory())
+            {
+                var blockchain = new Blockchain(tmpDir.Path);
+                blockchain.InsertBlock("New data");
+                Assert.AreEqual(1, blockchain.LastBlock.Height);
+                Assert.IsNotNull(blockchain.LastBlock.PreviousHash);
+                Assert.AreEqual("New data", blockchain.LastBlock.Data);
+            }
         }
         [TestMethod]
         public void TestGetBlock()
         {
-            var blockchain = CreateBlockchain();
-            blockchain.InsertBlock("New data 1");
-            blockchain.InsertBlock("New data 2");
-            var block = blockchain.GetBlock(1);
-            Assert.AreEqual(1, block.Height);
-            Assert.AreEqual(blockchain.LastBlock.PreviousHash, block.Hash);
-            Assert.AreEqual("New data 1", block.Data);
+            using (var tmpDir = new TempDirectory())
+            {
+                var blockchain = new Blockchain(tmpDir.Path);
+                blockchain.InsertBlock("New data 1");
+                blockchain.InsertBlock("New data 2");
+                var block = blockchain.GetBlock(1);
+                Assert.AreEqual(1, block.Height);
+                Assert.AreEqual(blockchain.LastBlock.PreviousHash, block.Hash);
+                Assert.AreEqual("New data 1", block.Data);
+            }
         }
         [TestMethod]
         public void TestValidBlockchain()
         {
-            var blockchain = CreateBlockchain();
-            blockchain.InsertBlock("New data 1");
-            blockchain.InsertBlock("New data 2");
-            blockchain.InsertBlock("New data 3");
-            
-            Assert.IsTrue(blockchain.IsValidChain());
+            using (var tmpDir = new TempDirectory())
+            {
+                var blockchain = new Blockchain(tmpDir.Path);
+                blockchain.InsertBlock("New data 1");
+                blockchain.InsertBlock("New data 2");
+                blockchain.InsertBlock("New data 3");
+
+                Assert.IsTrue(blockchain.IsValidChain());
+            }
+        }
+        [TestMethod]
+        public void TestPersistantBlockchain()
+        {
+            using (var tmpDir = new TempDirectory())
+            {
+                var blockchain = new Blockchain(tmpDir.Path);
+                blockchain.InsertBlock("New data 1");
+                blockchain.InsertBlock("New data 2");
+                blockchain.InsertBlock("New data 3");
+
+                var cpyBlockchain = new Blockchain(blockchain.DbPath);
+                Assert.AreEqual(3, cpyBlockchain.LastBlock.Height);
+            }
         }
     }
 }
