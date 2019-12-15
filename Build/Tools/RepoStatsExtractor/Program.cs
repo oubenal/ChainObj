3856d0eea3e217e843d0f321eb5f44e694615492
Redouane <oubenal@gmail.com>
2019-12-15 19:45:43 +0100

    rev Octokit

--- Build/Tools/RepoStatsExtractor/Program.cs
+++ Build/Tools/RepoStatsExtractor/Program.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using System.IO;
 
 namespace RepoStatsExtractor
 {
   class Program
   {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
       var gitShell = new GitShell(@"C:\RandD\dotnet-standard");
       gitShell.ShowVersion();
-      var commits = CommitInfo.ParseGitShellResult(gitShell.GetAllCommitStats());
+      //var commits = CommitInfo.ParseGitShellResult(gitShell.GetAllCommitStats());
+
     }
   }
 }
